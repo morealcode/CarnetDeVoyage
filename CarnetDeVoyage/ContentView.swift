14b63fb0df7836dc26e802d6b60fbd9fdeecc937
morealcode <235274488+morealcode@users.noreply.github.com>
2026-06-01 12:11:01 +0200

    Added some text

--- CarnetDeVoyage/ContentView.swift
+++ CarnetDeVoyage/ContentView.swift
@@ -14,6 +14,8 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Alexis")
+            Text("Breakdance")
+            Text("aka Bboy Moreal")
         }
         .padding()
     }
