8c6f7bb076db4837ba2286c8cd76dfcb14ae96aa
morealcode <235274488+morealcode@users.noreply.github.com>
2026-06-01 11:19:39 +0200

    Initial Commit

---
+++ CarnetDeVoyage/CarnetDeVoyageApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarnetDeVoyageApp.swift
+//  CarnetDeVoyage
+//
+//  Created by apprenant130 on 01/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct CarnetDeVoyageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
