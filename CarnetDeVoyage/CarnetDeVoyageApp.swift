2dfa8847ad56d4918b30d58ca4c8812d68456d51
morealcode <235274488+morealcode@users.noreply.github.com>
2026-06-02 15:30:03 +0200

    Hide scrollbar

--- CarnetDeVoyage/CarnetDeVoyageApp.swift
+++ CarnetDeVoyage/CarnetDeVoyageApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct CarnetDeVoyageApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            CarteLieu()
         }
     }
 }
--- CarnetDeVoyage/CarteLieu.swift
+++ CarnetDeVoyage/CarteLieu.swift
@@ -144,6 +144,7 @@ struct CarteLieu: View {
                 }
             }
         }
+        .scrollIndicators(.hidden)
     }
 }
 
