08b942f9a335b3fce8a746e87f4d869b4e121e99
morealcode <235274488+morealcode@users.noreply.github.com>
2026-06-06 21:32:08 +0200

    Add PlaceDetailView

--- CarnetDeVoyage/Components/PlaceDetailView.swift
+++ CarnetDeVoyage/Components/PlaceDetailView.swift
@@ -8,20 +8,114 @@
 import SwiftUI
 
 struct PlaceDetailView: View {
-    
-    @State var place: Place
-    
+
+    @Binding var place: Place
+    @State private var isShowingSheet: Bool = true
+
     var body: some View {
-        VStack{
-            Text(place.name)
-            Text(place.resume)
+        ZStack(alignment: .bottom) {
+            Color(.white.opacity(0))
+
+            VStack(alignment: .leading, spacing: 20) {
+                HStack {
+                    Text(place.name)
+                        .font(.title)
+                        .bold()
+                    Spacer()
+                    Text("\(place.budget) €")
+                        .font(.title2)
+                        .bold()
+                }
+
+                HStack(spacing: 14) {
+                    ForEach(place.tags, id: \.self) { tag in
+                        Label(tag.capitalized, systemImage: "checkmark")
+                            .padding(.vertical, 2)
+                            .padding(.horizontal, 8)
+                            .font(.callout)
+
+                            .padding(8)
+                            .background(
+                                RoundedRectangle(cornerRadius: 30)
+                                    .fill(.white)
+                            )
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 30)
+                                    .stroke(
+                                        .gray.opacity(0.3),
+                                        lineWidth: 1
+                                    )
+                            )
+                            .labelStyle(.titleOnly)
+                    }
+                }
+
+                Text("Resume")
+                    .font(.title3)
+                    .bold()
+
+                Text(place.resume)
+
+                HStack {
+
+                    Spacer()
+                    
+                    // Add/Remove place from planning
+                    Button(
+                        place.willVisit ? "Planned" : "Not planned",
+                        systemImage: place.willVisit ? "checkmark" : "multiply"
+                    ) {
+
+                        print("Update planning")
+                        place.willVisit.toggle()
+
+                    }
+                    .buttonStyle(.glassProminent)
+                        .tint(place.willVisit ? .primary : .secondary)
+                        .controlSize(.large)
+                    
+                    // Add/Remove place from memories
+                    Button(
+                        place.isVisited ? "Visited" : "Not visited",
+                        systemImage: place.isVisited ? "checkmark" : "multiply"
+                    ) {
+
+                        print("Update planning")
+                        place.isVisited.toggle()
+
+                    }
+                    .buttonStyle(.glassProminent)
+                        .tint(place.isVisited ? .primary : .secondary)
+                        .controlSize(.large)
+                    
+                    Spacer()
+
+                }
+
+            }
+            .padding(20)
+            .background(.white)
+            .clipShape(
+                UnevenRoundedRectangle(
+                    topLeadingRadius: 20,
+                    bottomLeadingRadius: 0,
+                    bottomTrailingRadius: 0,
+                    topTrailingRadius: 20
+                )
+            )
         }
-        .navigationTitle(place.name)
+        .background(
+            Image(place.image)
+                .resizable()
+                .scaledToFill()
+                .ignoresSafeArea(edges: .top)
+        )
     }
 }
 
 #Preview {
-    NavigationStack{
-        PlaceDetailView(place: Place.example)
+    @Previewable @State var place: Place = Place.example
+    NavigationStack {
+        PlaceDetailView(place: $place)
     }
 }
--- CarnetDeVoyage/Views/DiscoverView.swift
+++ CarnetDeVoyage/Views/DiscoverView.swift
@@ -12,134 +12,137 @@ struct DiscoverView: View {
     @Binding var places: [Place]
 
     var body: some View {
-        ScrollView {
+        NavigationStack{
+            ScrollView {
 
-            VStack(alignment: .leading) {
-                Text("What's next 👀")
-                    .font(.title)
-                    .bold()
-                    .padding()
+                VStack(alignment: .leading) {
+                    Text("What's next 👀")
+                        .font(.title)
+                        .bold()
+                        .padding()
 
-                ScrollView(.horizontal) {
-                    HStack(spacing: 16) {
-                        
-                        ForEach(Place.continents, id: \.self) { continent in
-                            Button {
-                                print("Select", continent)
-                            } label: {
-                                HStack(spacing: 8) {
-                                    Image(continent.lowercased())
-                                        .resizable()
-                                        .scaledToFill()
-                                        .frame(width: 40, height: 40)
-                                        .clipShape(Circle())
+                    ScrollView(.horizontal) {
+                        HStack(spacing: 16) {
+                            
+                            ForEach(Place.continents, id: \.self) { continent in
+                                Button {
+                                    print("Select", continent)
+                                } label: {
+                                    HStack(spacing: 8) {
+                                        Image(continent.lowercased())
+                                            .resizable()
+                                            .scaledToFill()
+                                            .frame(width: 40, height: 40)
+                                            .clipShape(Circle())
 
-                                    Text(continent)
-                                        .foregroundStyle(.black)
-                                        .padding(.trailing, 15)
+                                        Text(continent)
+                                            .foregroundStyle(.black)
+                                            .padding(.trailing, 15)
+                                    }
+                                    .padding(3)
+                                    .background(
+                                        RoundedRectangle(cornerRadius: 30)
+                                            .fill(.white)
+                                    )
+                                    .overlay(
+                                        RoundedRectangle(cornerRadius: 30)
+                                            .stroke(
+                                                .gray.opacity(0.3),
+                                                lineWidth: 1
+                                            )
+                                    )
                                 }
-                                .padding(3)
-                                .background(
-                                    RoundedRectangle(cornerRadius: 30)
-                                        .fill(.white)
-                                )
-                                .overlay(
-                                    RoundedRectangle(cornerRadius: 30)
-                                        .stroke(
-                                            .gray.opacity(0.5),
-                                            lineWidth: 1
-                                        )
-                                )
+
                             }
-
                         }
+                        .padding()
                     }
-                    .padding()
-                }
 
-                Group {
+                    Group {
 
-                    ForEach($places) { $place in
+                        ForEach($places) { $place in
 
-                        VStack {
+                            VStack {
 
-                            HStack {
                                 HStack {
-                                    Image(systemName: "star.fill")
-                                        .foregroundStyle(.yellow)
-                                    Text("5.0")
-                                }
-                                .padding()
-                                .glassEffect()
-                                Spacer()
-                                Image(systemName: "heart.fill")
-                                    .foregroundStyle(.white)
+                                    HStack {
+                                        Image(systemName: "star.fill")
+                                            .foregroundStyle(.yellow)
+                                        Text(String(place.review))
+                                    }
                                     .padding()
                                     .glassEffect()
-                            }
-
-                            Spacer()
-
-                            HStack {
-                                VStack(alignment: .leading, spacing: 8) {
-                                    Label(
-                                        "\(place.city), \(place.pays)",
-                                        systemImage: "mappin.and.ellipse"
-                                    )
-                                    .bold()
-
-                                    Text(place.name)
-                                        .font(.title2)
-                                        .bold()
+                                    Spacer()
+                                        
+                                    Button("Like", systemImage: "heart.fill", action: {
+                                        place.isLiked.toggle()
+                                    })
+                                    .padding()
+                                    .labelStyle(.iconOnly)
+                                    .foregroundStyle(place.isLiked ? .red : .white)
+                                    .glassEffect()
                                 }
-                                .foregroundStyle(.white)
 
                                 Spacer()
 
-                                Button(
-                                    "See details",
-                                    systemImage: "arrow.up.right",
-                                    action: {
-                                        print("Show details for", place.name)
+                                HStack {
+                                    VStack(alignment: .leading, spacing: 8) {
+                                        Label(
+                                            "\(place.city), \(place.pays)",
+                                            systemImage: "mappin.and.ellipse"
+                                        )
+                                        .bold()
+
+                                        Text(place.name)
+                                            .font(.title2)
+                                            .bold()
                                     }
-                                )
-                                .foregroundStyle(.primary)
-                                .labelStyle(.iconOnly)
-                                .padding()
-                                .glassEffect()
+                                    .foregroundStyle(.white)
+
+                                    Spacer()
+                                    
+                                    NavigationLink {
+                                        PlaceDetailView(place: $place)
+                                    } label: {
+                                        Image(systemName: "arrow.up.right")
+                                    }
+                                    .padding()
+                                    .glassEffect()
+                                    .foregroundStyle(.primary)
+
+                                }
 
                             }
-
-                        }
-                        .frame(maxWidth: .infinity)
-                        .frame(height: 270)
-                        .padding()
-                        .background(
-                            Image(place.image)
-                                .resizable()
-                                .scaledToFill()
-                                .overlay(
-                                    LinearGradient(
-                                        colors: [
-                                            .black.opacity(0),
-                                            .black.opacity(0.5),
-                                            .black.opacity(0.6),
-                                        ],
-                                        startPoint: .center,
-                                        endPoint: .bottom
+                            .frame(maxWidth: .infinity)
+                            .frame(height: 270)
+                            .padding()
+                            .background(
+                                Image(place.image)
+                                    .resizable()
+                                    .scaledToFill()
+                                    .overlay(
+                                        LinearGradient(
+                                            colors: [
+                                                .black.opacity(0),
+                                                .black.opacity(0.5),
+                                                .black.opacity(0.6),
+                                            ],
+                                            startPoint: .center,
+                                            endPoint: .bottom
+                                        )
                                     )
-                                )
 
-                        )
-                        .clipShape(.rect(cornerRadius: 25))
+                            )
+                            .clipShape(.rect(cornerRadius: 25))
+                        }
+
                     }
-
+                    .padding(.horizontal)
+                    .padding(.vertical, 8)
                 }
-                .padding(.horizontal)
-                .padding(.vertical, 8)
             }
+            .scrollIndicators(.hidden)
         }
-        .scrollIndicators(.hidden)
     }
 }
 
